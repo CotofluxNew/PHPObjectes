7f2e48be1fe240ac2d38c82de14b8e4cb505247b
CotofluxNew <cotoflux@gmail.com>
2023-02-20 12:21:35 +0100

    Ejercicio corregido

--- Nivell2/index.php
+++ Nivell2/index.php
@@ -5,6 +5,8 @@ require_once('PokerDice.php');
     $juego  = new PokerDice();
     $numeroTiradas = $juego->getTotalThrows();
 
+    echo "Las 5 tiradas son: ";
+
     for($i=0; $i<$numeroTiradas; $i++){
         $tirada = $juego->shapeName(); 
         echo $tirada. "<br/>";
